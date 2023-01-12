5d7083c00246e9d6e0ddf7de1e77c75d326e02ff
rgazizulin174 <rgazizulin174@gmail.com>
2023-01-12 23:44:10 +0500

    task 1 - 2

--- my_project_part_1/tours/models.py
+++ my_project_part_1/tours/models.py
@@ -13,10 +13,11 @@ class Tour(models.Model):
 
 
 class Review(models.Model):
-    # TODO author  Имя пользователя  Короткий текст (максимальная длинна 20 символов)
-    # TODO tour  Экскурсия Связь с моделью Tour (foreign key c каскадным удалением)
-    # TODO content  Текст отзыва Длинный текст
-    # TODO rate  Звездочки Маленькое целое число
-    # TODO published_at  Время публикации  Дата и время
-    # TODO is_published  Отзыв опубликован Логическое
-    pass
+    author = models.CharField(max_length=20)
+    tour = models.ForeignKey(Tour, on_delete=models.CASCADE)
+    content = models.TextField()
+    rate = models.SmallIntegerField()
+    published_at = models.DateTimeField(auto_now_add=True, null=True)
+    is_published = models.BooleanField(default=False)
+
+
