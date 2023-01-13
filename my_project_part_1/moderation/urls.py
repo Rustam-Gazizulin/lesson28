c0cd1fb3270fba725e86f956e0971ab2ef2e6d9f
rgazizulin174 <rgazizulin174@gmail.com>
2023-01-13 22:30:00 +0500

    add moderation

--- my_project_part_1/moderation/urls.py
+++ my_project_part_1/moderation/urls.py
@@ -1,3 +1,9 @@
 # TODO настраиваем urls здесь
+from django.urls import path
 
-urlpatterns = []
+from moderation import views
+
+urlpatterns = [
+    path('listreview/', views.ReviewListView.as_view(), name='review'),
+    path('feedback-delete/<int:pk>/', views.ReviewDeleteView.as_view(), name='delete'),
+]
--- my_project_part_1/moderation/views.py
+++ my_project_part_1/moderation/views.py
@@ -1,9 +1,36 @@
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
-from django.views.generic import DeleteView
+from django.views.generic import DeleteView, ListView
+
+from moderation.models import Review
+
+class ReviewListView(ListView):
+    model = Review
+
+    def get(self, request, *args, **kwargs):
+        super().get(request, *args, **kwargs)
+
+        response = []
+
+        for rev in self.object_list:
+            response.append({
+                "author": rev.author,
+                "content": rev.content,
+                "tour": rev.tour_id
+
+            })
+
+        return JsonResponse(response, safe=False)
 
 
-# TODO здесь необходимо реализовать CBV в соответствии с заданием
+
 @method_decorator(csrf_exempt, name="dispatch")
 class ReviewDeleteView(DeleteView):
-    pass
+    model = Review
+    success_url = '/'
+
+    def delete(self, request, *args, **kwargs):
+        super().delete(request, *args, **kwargs)
+
+        return JsonResponse({'status': 'ok'}, status=200)
